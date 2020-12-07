2221005d0a2ec7ba27cba28745d2684a21ecd6ee
Caroline E <caroline.esquivel01@gmail.com>
2020-12-07 09:42:20 -0600

    user details

--- src/main/java/com/codeup/spring/repository/UserRepository.java
+++ src/main/java/com/codeup/spring/repository/UserRepository.java
@@ -3,5 +3,6 @@ package com.codeup.spring.repository;
 import com.codeup.spring.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User, Long>{
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUsername(String username);
 }
