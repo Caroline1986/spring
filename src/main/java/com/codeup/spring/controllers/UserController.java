91d67a68f4e6ca7800fe18d7b0eebd8c7da51b75
Caroline E <caroline.esquivel01@gmail.com>
2020-12-07 16:39:47 -0600

    creating user profile page

--- src/main/java/com/codeup/spring/controllers/UserController.java
+++ src/main/java/com/codeup/spring/controllers/UserController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
@@ -31,5 +32,11 @@ public class UserController {
         user.setPassword(hash);
         usersDao.save(user);
         return "redirect:/login";
+    }
+
+    @GetMapping("/profile/{id}")
+    public String userProfile(@PathVariable long id, Model model) {
+        model.addAttribute("user", usersDao.getOne(id));
+        return "/profile";
     }
 }
