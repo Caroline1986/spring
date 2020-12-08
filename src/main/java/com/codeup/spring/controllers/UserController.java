33590c1cab5c73aad3902eef0b92c96f76b153be
Caroline E <caroline.esquivel01@gmail.com>
2020-12-08 11:28:36 -0600

    add needed files for cods

--- src/main/java/com/codeup/spring/controllers/UserController.java
+++ src/main/java/com/codeup/spring/controllers/UserController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
@@ -33,4 +34,9 @@ public class UserController {
         return "redirect:/login";
     }
 
+    @GetMapping("/profile/{id}")
+    public String userProfile(@PathVariable long id, Model model) {
+        model.addAttribute("user", usersDao.getOne(id));
+        return "/profile";
+    }
 }
