b0e21e598c871519c964a208e73eca17a2b56482
Caroline E <caroline.esquivel01@gmail.com>
2021-01-26 21:46:16 -0600

    paypal properties in application.properties

---
+++ src/main/java/com/codeup/spring/configuration/PaypalConfig.java
@@ -0,0 +1,15 @@
+package com.codeup.spring.configuration;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class PaypalConfig {
+
+    @Value("${paypal.client.id}")
+    private String clientId;
+    @Value("${paypal.client.secret}")
+    private String clientSecret;
+    @Value("${paypal.mode}")
+    private String mode;
+}
