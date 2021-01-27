d7c97284c03b9c105373c8bde423ec0b4d189f2d
Caroline E <caroline.esquivel01@gmail.com>
2021-01-26 23:20:13 -0600

    creating payment mapping for paypal form

--- src/main/java/com/codeup/spring/controllers/PaypalController.java
+++ src/main/java/com/codeup/spring/controllers/PaypalController.java
@@ -1,9 +1,13 @@
 package com.codeup.spring.controllers;
 
 import com.codeup.spring.services.PaypalService;
+import com.paypal.api.payments.Links;
+import com.paypal.api.payments.Payment;
+import com.paypal.base.rest.PayPalRESTException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
 @Controller
 public class PaypalController {
@@ -11,8 +15,28 @@ public class PaypalController {
     @Autowired
     PaypalService service;
 
+    public static final String SUCCESS_URL = "pay/success";
+    public static final String CANCEL_URL = "pay/cancel";
+
     @GetMapping("/")
-    public String home(){
+    public String home() {
         return "home";
     }
+
+    @GetMapping("/pay")
+    public String payment(@ModelAttribute("order") Order order) {
+        try {
+            Payment payment = service.createPayment(order.getPrice(), order.getCurrency(), order.getMethod(), order.getIntent(), order.getDescription(), "http://localhost:8080/" + CANCEL_URL, "http://localhost:8080/" + SUCCESS_URL);
+            for (Links link : payment.getLinks()) {
+                if (link.getRel().equals("approval_url")) {
+                    return "redirect:" + link.getHref();
+                }
+            }
+        } catch (PayPalRESTException e) {
+            e.printStackTrace();
+        }
+        return "redirect:/";
+    }
+
+
 }
